e2e11f780ddd06e26a25fb2433a2d951e7ce9437
Michal Wrona <wronamichal01@gmail.com>
2022-03-06 15:15:41 +0100

    Added Video, Playlist models and saving videos from playlist

--- api/models.py
+++ api/models.py
@@ -1,3 +1,36 @@
+import pytube
 from django.db import models
+from django.contrib.auth import get_user_model
+from pytube import YouTube
+import pytube
+from pytube.exceptions import VideoUnavailable
 
-# Create your models here.
+
+class Playlist(models.Model):
+    url_playlist_yt = models.URLField(null=False, blank=False)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        playlist = pytube.Playlist(url=self.url_playlist_yt)
+        return f'{playlist.title} - {self.user}'
+
+    def save(self, *args, **kwargs):
+        try:
+            playlist = pytube.Playlist(url=self.url_playlist_yt)
+        except VideoUnavailable:
+            return
+        else:
+            super().save(*args, **kwargs)
+            for video_url in playlist.video_urls:
+                video = Video(url_video_yt=video_url, user=self.user, playlist=self)
+                video.save()
+
+
+class Video(models.Model):
+    url_video_yt = models.URLField(null=False, blank=False)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        video_title = YouTube(url=self.url_video_yt).title
+        return f'{video_title} - {self.user}'
--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
 from rest_framework import routers
 
+
 router = routers.DefaultRouter()
