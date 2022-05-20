400b883836ddb1fe65abd2bb439b007c9bfb450b
Michał Wrona <wronamichal01@gmail.com>
2022-05-20 17:49:48 +0200

    Added CORS to MIDDLEWARE

--- yt_downloader/settings.py
+++ yt_downloader/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'yt_downloader.urls'
