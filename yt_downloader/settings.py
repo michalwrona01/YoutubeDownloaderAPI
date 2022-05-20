681d745eabebc5e51c0c4cca7b8fd64f49a96d2b
Michał Wrona <wronamichal01@gmail.com>
2022-05-20 17:27:25 +0200

    Update static dirs

--- yt_downloader/settings.py
+++ yt_downloader/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
--- yt_downloader/urls.py
+++ yt_downloader/urls.py
@@ -21,5 +21,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls))
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
